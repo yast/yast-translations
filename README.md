1f7f9bd1a49ba439d443096463802d40b84204ab
Stanislav Brabec <sbrabec@suse.com>
2016-09-13 19:42:18 +0200

    Update update-tool.sh
    Do yast-checkout if needed.
    Clone yast-meta temporarily on first checkout.
    Merge final pot files from po-parts.
    Skip update if no changes occur.

--- README.md
+++ README.md
@@ -3,5 +3,3 @@ All YaST translations
 run tools/update-tool.sh to update po files
 
 tools/yast-check-lcn-import.sh can be used to check consistency
-
-you need to clone yast-meta first
--- tools/update-tool.sh
+++ tools/update-tool.sh
@@ -1,37 +1,94 @@
-#!/bin/bash -e
+#!/bin/bash
 
-if ! test -e ../devtools/build-tools/scripts/y2makepot ; then
-	echo "Please checkout yast properly"
+# Note: Do not call this script inside translations repo in read-only checkout!
+# Make R/W checkout instead.
+
+set -o errexit
+
+if test -f "update-tool.sh" ; then
+	cd ..
+fi
+if ! test -f "tools/update-tool.sh" ; then
+	echo "Please call this script from yast-translations top directory."
 	exit 1
 fi
+WORKDIR=$PWD
 
-cd ..
-meta/y2m pull
+YAST_CHECKOUT=
+for DIR in ~ "$PWD"/.. ; do
+	if test -d $DIR/yast-checkout ; then
+		YAST_CHECKOUT=$DIR/yast-checkout
+	fi
+done
+if test -z "$YAST_CHECKOUT" ; then
+	echo "No yast-checkout find. Do you want to do the checkout now? (Y/n)"
+	read
+	if test "$REPLY" = n ; 	then
+		exit 1
+	fi
+	mkdir ../yast-checkout-temp
+	cd ../yast-checkout-temp
+	git clone https://github.com/yast/yast-meta
 
-TRANDIR=$PWD/translations/po
-y2makepot=$PWD/devtools/build-tools/scripts/y2makepot
+	mkdir ~/yast-checkout
+	cd ~/yast-checkout
+	$OLDPWD/../yast-checkout-temp/yast-meta/y2m read-only ALL
+	YAST_CHECKOUT=$PWD
+
+	rm -rf $OLDPWD/../yast-checkout-temp
+fi
+
+TRANDIR=$WORKDIR/po
+TRANPARTS=$WORKDIR/po-parts
+Y2MAKEPOT=$YAST_CHECKOUT/devtools/build-tools/scripts/y2makepot
+Y2M=$YAST_CHECKOUT/meta/y2m
+
+cd $YAST_CHECKOUT
+
+$Y2M pull
 
 shopt -s nullglob
+
 for DIR in * ; do
 	[ "$DIR" != 'translations' ] || continue
 	pushd $DIR
-	$y2makepot
+	$Y2MAKEPOT
 	for POT in *.pot ; do
 		DOMAIN=${POT%.pot}
 		if grep -q "^$DOMAIN.pot" $TRANDIR/OBSOLETE_POT_FILES; then
 			continue
 		fi
-		mkdir -p $TRANDIR/$DOMAIN
-		cp -a $POT $TRANDIR/$DOMAIN/
-		pushd $TRANDIR/$DOMAIN
-		git add $POT
-		for PO in *.po ; do
-			msgmerge --previous --lang=${PO%.po} $PO $POT -o $PO.new
-			mv $PO.new $PO
-			git add $PO
-		done
-		git commit -m "Automatic update of $DOMAIN."
-		popd
+		mkdir -p $TRANPARTS/$DOMAIN
+		cp -a $POT $TRANPARTS/$DOMAIN/$DIR.pot
 	done
+	popd
+done
+
+cd $TRANPARTS
+for DOMAIN in * ; do
+	mkdir -p $TRANDIR/$DOMAIN
+	# This is an ugly hack: Use the newest file as source of header
+	msgcat --use-first $(ls -1 --sort=time $DOMAIN/*.pot) -o $TRANDIR/$DOMAIN/$DOMAIN.pot.new
+	pushd $TRANDIR/$DOMAIN
+	# prevent re-committing when pot file uses different line format
+	msgcat $DOMAIN.pot -o $DOMAIN.pot.tmp
+	sed '1,11{/^"POT-Creation-Date:/d};1,12{/^"PO-Revision-Date:/d}' <$DOMAIN.pot.tmp >$DOMAIN.pot.nodate
+	sed '1,11{/^"POT-Creation-Date:/d};1,12{/^"PO-Revision-Date:/d}' <$DOMAIN.pot.new >$DOMAIN.pot.new.nodate
+	if cmp -s $DOMAIN.pot.nodate $DOMAIN.pot.new.nodate ; then
+		echo "No changes in $DOMAIN.pot. Skipping update."
+		rm *.tmp *.nodate *.new
+		popd
+		continue
+	fi
+	echo "Updating $DOMAIN."
+	rm *.tmp *.nodate
+	mv $DOMAIN.pot.new $DOMAIN.pot
+	git add $DOMAIN.pot
+	for PO in *.po ; do
+		msgmerge --previous --lang=${PO%.po} $PO $DOMAIN.pot -o $PO.new
+		mv $PO.new $PO
+		git add $PO
+	done
+	git commit -m "Automatic update of $DOMAIN."
 	popd
 done
